e9fddf3058c9ee80b21f8181439cec9f7b1ec289
eric valcik <eric.valcik@gmail.com>
2023-05-10 23:21:38 +0200

    Input on PC working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,33 @@
-export default function Home() {
-  return (
-    <>
-      <input type="checkbox" id="toggle" name="toggle" />
-      <label htmlFor="toggle">Hide the overlay to see what&apos;s inside</label>
+"use client";
 
-      <main>
-        <div id="a"></div>
-        <div id="b"></div>
-      </main>
-    </>
+import { useCallback, useEffect, useState } from "react";
+
+export default function Home() {
+  const [change, setChange] = useState(false);
+
+  const darken = useCallback((color: string): string => {
+    if (color === "#222222") return color;
+    let r = parseInt(color.slice(1, 3), 16);
+    r = r > 44 ? r - 10 : 34;
+    return `#${r.toString(16)}${r.toString(16)}${r.toString(16)}`;
+  }, []);
+
+  useEffect(() => {
+    let r = document.querySelector(":root");
+    let color = "#ffffff";
+    const interval = setInterval(() => {
+      if (!r) return;
+      r.style.setProperty("--input-bg-color", color);
+      color = darken(color);
+    }, 20);
+    return () => clearInterval(interval);
+  }, [change, darken]);
+
+  return (
+    <main>
+      <div id="b">
+        <input type="text" onChange={() => setChange(!change)} />
+      </div>
+    </main>
   );
 }
